965600867c247234be5c6d67a94b57eb01146b7d
TMP <tmpdz7820@gmail.com>
2025-11-19 17:18:26 +0000

    build framework

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use App\Presentation\Http\Controllers\TestController;
 
 // SPA Fallback Route - Catches all routes for Vue Router
 // This allows Vue Router to handle client-side routing
-// Route::any('/{any}', [AppController::class, 'index'])->where('any', '.*');
+Route::any('/{any}', [AppController::class, 'index'])->where('any', '.*');
 // routes/web.php hoặc routes/api.php
-Route::get('/test/create-product', [TestController::class, 'createProduct']);
+// Route::get('/test/create-product', [TestController::class, 'createProduct']);
